6b091644ffbbc27bc7218b7ca0ea61fd63a59b22
Adam Schmideg <adam@schmideg.net>
2017-12-08 14:44:32 +0100

    Day 1 solved

--- src/advent2017/core.clj
+++ src/advent2017/core.clj
@@ -1,5 +1,15 @@
-(ns advent2017.core)
+(ns advent2017.core
+  (:require [clojure.edn :refer [read-string]]))
 
-(defn captcha [s])
+(defn captcha [s]
+  (let [rotated (conj (butlast s) (last s))
+        digits (map
+                 (fn [x y]
+                   (if (= x y)
+                     (read-string (str x))
+                     0))
+                 s
+                 rotated)]
+    (apply + digits)))
 
 
--- test/advent2017/core_test.clj
+++ test/advent2017/core_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [advent2017.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
 (deftest d01
   (are [in out] (= (captcha in) out)
     "1122" 3
