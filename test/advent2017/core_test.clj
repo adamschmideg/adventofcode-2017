1c4e1bf382292cbcef939ffb25a867b270b00cb2
Adam Schmideg <adam@schmideg.net>
2017-12-08 14:23:12 +0100

    Tests for day 1

--- test/advent2017/core_test.clj
+++ test/advent2017/core_test.clj
@@ -5,3 +5,12 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest d01
+  (are [in out] (= (captcha in) out)
+    "1122" 3
+    "1111" 4
+    "1234" 0
+    "91212129" 9))
+
+(run-tests)
